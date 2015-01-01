77450b7b46ca15c66ccd3561bd90042aa9696cc8
mokkunp <mokkunp@gmail.com>
2015-01-02 04:07:30 +0900

    Add cask and some emacs package

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,19 @@
+
+(package-initialize)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+
+(package-initialize)
+
+(unless (require 'use-package nil t)
+  (defmacro use-package (&rest args)))
+
+(use-package pallet)
+(use-package ruby-mode)
+(use-package dash)
+(use-package magit)
+
 ;カーソル移動
 (global-set-key "\C-h" 'delete-backward-char)
 
